8da6370a3cea39d45f3bf4ae88964a08c67f4e2d
RuizLenato <ruizlenato@proton.me>
2023-11-28 12:55:33 -0300

    Chore: bludv support temporarily removed.

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func main() {
 
 	http.HandleFunc("/", handler.HandlerIndex)
 	http.HandleFunc("/indexers/comando_torrents", indexers.HandlerComandoIndexer)
-	http.HandleFunc("/indexers/bludv", indexers.HandlerBluDVIndexer)
 
 	err := http.ListenAndServe(":7006", nil)
 	if err != nil {
